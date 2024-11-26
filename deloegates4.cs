8cb69a6690247ac25fec7079cda4cdd2a3db0828
superdavid123 <168566456+superdavid123@users.noreply.github.com>
2024-11-26 17:43:30 +0100

    Update deloegates4.cs

--- deloegates4.cs
+++ deloegates4.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
 
+class Program
+{
+	static void Main()
+	{
+		List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+		Predicate<int> je = Je;
+
+		List<int> cisla = numbers.FindAll(je);
+		Console.WriteLine(string.Join(", ", cisla));
+	}
+	static bool Je(int cislicko)
+	{
+		return cislicko % 2 == 0;
+	}
+}
