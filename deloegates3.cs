e908f8e199cbd47d3f6e72ae37d7a89543c663a9
superdavid123 <168566456+superdavid123@users.noreply.github.com>
2024-11-26 17:28:28 +0100

    Update deloegates3.cs

--- deloegates3.cs
+++ deloegates3.cs
@@ -1,1 +1,14 @@
+using System;
 
+class Program
+{
+	static void Main()
+	{
+		Func<int, int, int> nasobeni = Nasobeni;
+		Console.WriteLine(nasobeni(5, 8));
+	}
+	static int Nasobeni(int x, int y)
+	{
+		return x * y;
+	}
+}
