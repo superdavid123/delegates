fc5d2c96b91e16297a0004d259acf3a499398594
superdavid123 <168566456+superdavid123@users.noreply.github.com>
2024-11-26 17:22:52 +0100

    Update deloegates2.cs

--- deloegates2.cs
+++ deloegates2.cs
@@ -1,28 +1,4 @@
-using System;
 
-<<<<<<< HEAD
-delegate int matematicoperace(int x, int y);
-
-class program
-{
-	static void Main()
-	{
-		matematicoperace oskarsmrdi = Add;
-		matematicoperace oskarnesmrdi = Substract;
-		Console.WriteLine( oskarsmrdi(3, 4) );
-		Console.WriteLine( oskarnesmrdi(3, 4) );
-
-	}
-	static int Add(int x, int y)
-	{
-		return x + y;
-	}
-	static int Substract(int x, int y)
-	{
-		return x - y;
-	}
-}
-=======
 class Program
 {
 	static void Main()
@@ -37,4 +13,3 @@ class Program
 		Console.WriteLine(message);
 	}
 }
->>>>>>> 87307bd8e8a91b530c893c5af7969f5a037935bb
